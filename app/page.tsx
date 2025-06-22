55bab01da4c7ddbfa6b3497841a3c0be2f81cada
Talha Abbas <talhaabbas556989@gmail.com>
2025-06-22 06:23:51 -0700

    add dynamic issue

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { navItems } from "@/data";
+export const dynamic = "force-dynamic";
+
 
 export default function Home() {
   return (
