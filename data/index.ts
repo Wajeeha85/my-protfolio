59580a1654175c07e24781de8feed84408177fa9
Wajeeha85 <wajeehaabbas85@gmail.com>
2025-12-24 23:05:46 +0500

    change a title

--- data/index.ts
+++ data/index.ts
@@ -49,7 +49,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building a JS Animation library",
+    title: "Currently Exploring Backend",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
@@ -83,7 +83,7 @@ export const projects = [
     title: "ChattApp",
     des: "Built a real-time chat app using Next.js, TailwindCSS,TypeScript and Firebase with auth, messaging, and live online status features.",
     img: "/Screenshot 2025-06-22 155125.png",
-    iconLists: ["/next.svg", "/firebase.svg", "/tail.svg",],
+    iconLists: ["/next.svg", "/firebase.svg", "/tail.svg",],e
     link: "https://github.com/Wajeeha85/Chattapp/tree/maste ",
   },
   {
